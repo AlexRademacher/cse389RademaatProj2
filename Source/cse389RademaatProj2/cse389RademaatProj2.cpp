518cc8d88c0cd74ca49f0fe2bf452df69f13edd0
Denwin123 <rademaat@miamioh.edu>
2025-11-04 08:58:52 -0500

    Initial commit

---
+++ Source/cse389RademaatProj2/cse389RademaatProj2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "cse389RademaatProj2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, cse389RademaatProj2, "cse389RademaatProj2" );
